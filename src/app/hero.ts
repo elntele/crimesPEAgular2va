c54637f00f780c9f92cf25baeee5e904e8996f5b
Jorge Candeias do Nascimento <elntele@gmail.com>
2017-03-02 02:02:54 -0300

    comit inicial

---
+++ src/app/hero.ts
@@ -0,0 +1,14 @@
+
+export class Hero{
+	id: number;
+	cidade: string;
+	data:string;
+	idade:number;
+	nome_vitima: string;
+	objeto_utilizado: string;
+	qtd_vitimas:string;
+	sexo:string;
+	tipo_crime:string;
+	status: string;
+}
+
